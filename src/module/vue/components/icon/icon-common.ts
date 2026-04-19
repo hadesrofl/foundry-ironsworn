5065fead3bb3a783ebde8a1384608326ea33e756
Rene Kremer <renejon90@googlemail.com>
2026-04-19 15:12:48 +0200

    fix: Update font awesome icons

--- src/module/vue/components/icon/icon-common.ts
+++ src/module/vue/components/icon/icon-common.ts
@@ -195,11 +195,16 @@ export namespace FontAwesome {
 		 * Set Font Awesome icon family
 		 */
 		'fa-style-family':
-			| 'Font Awesome 6 Free'
-			| 'Font Awesome 6 Pro'
-			| 'Font Awesome 6 Duotone'
-			| 'Font Awesome 6 Brands'
-			| 'Font Awesome 6 Sharp'
+		| 'Font Awesome 6 Free'
+		| 'Font Awesome 6 Pro'
+		| 'Font Awesome 6 Duotone'
+		| 'Font Awesome 6 Brands'
+		| 'Font Awesome 6 Sharp'
+		| 'Font Awesome 7 Free'
+		| 'Font Awesome 7 Pro'
+		| 'Font Awesome 7 Duotone'
+		| 'Font Awesome 7 Brands'
+		| 'Font Awesome 7 Sharp'
 		// Set Font Awesome icon style 	Any valid Font Awesome style weight
 		'fa-style': 900 | 400 | 100 | 300
 		/**
